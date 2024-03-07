0a99ab6150ff336dd0322aa958802a5d1df0be97
miirshe <miirshe@gmail.com>
2024-03-07 09:16:33 +0300

    feat: add -kinde authentication like sign in, sign up, and sign out

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,23 +1,39 @@
 import Link from "next/link";
 import { ThemeToggle } from "./Theme-toggle";
 import { Button } from "@/components/ui/button";
-
-export default function Navbar(){
-    return (
-        <nav className="bg-background border-b h-[10vh] flex items-center">
-            <div className="container flex items-center justify-between">
-                <Link href="">
-                    <h1 className="text-3xl tracking-widest font-bold">NoteNado</h1>
-                </Link>
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
+  return (
+    <nav className="bg-background border-b h-[10vh] flex items-center">
+      <div className="container flex items-center justify-between">
+        <Link href="">
+          <h1 className="text-3xl tracking-widest font-bold">NoteNado</h1>
+        </Link>
+        <div className="flex items-center gap-x-5">
+          <ThemeToggle />
+         {
+            (await isAuthenticated()) ? (
+                <LogoutLink> <Button>Log out</Button></LogoutLink>
+            )
+            : (
                 <div className="flex items-center gap-x-5">
-                    <ThemeToggle/>
-                    <div className="flex items-center gap-x-5">
-                        <Button>Sign In</Button>
-                        <Button variant="secondary">Sign Up</Button>
-                    </div>
-                </div>
-            </div>
-
-        </nav>
-    )
+                <LoginLink>
+                  <Button>Sign In</Button>
+                </LoginLink>
+                <RegisterLink>
+                  <Button variant="secondary">Sign Up</Button>
+                </RegisterLink>
+              </div>
+            )
+         }
+        </div>
+      </div>
+    </nav>
+  );
 }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard(){
+    return (
+        <main>
+            <h1>Hello from Dashboard</h1>
+        </main>
+    )
+}
